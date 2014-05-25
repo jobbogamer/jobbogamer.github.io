fa784758185c151a99d9bfb4b820efe31928aaf1
Josh Asch <josh.asch@gmail.com>
2014-05-25 21:45:06 +0100

    Test out facebook sharing

---
+++ javascript/facebook.js
@@ -0,0 +1,32 @@
+window.fbAsyncInit = function() {
+	FB.init({
+		appId	: '700174693381750',
+		xfbml	: true,
+		version	: 'v2.0'
+	});
+};
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+function sharePage(url) {
+	FB.ui(
+		{
+			method: 'share',
+			href: url,
+		},
+		function(response) {
+			if (response && !response.error_code) {
+				alert('Posting completed.');
+			} else {
+				alert('Error while posting.');
+			}
+		}
+	);
+}
